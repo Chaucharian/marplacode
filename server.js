db21f8b575b5005ffdeb0489923d9251c73c641d
Agustin Salinas <chaucharian@gmail.com>
2019-07-31 15:34:03 -0300

    trying using path dependency

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const port = process.env.PORT || 1234;
 const app = express();
+path = require("path");
 
 // the __dirname is the current directory from where the script is running
-app.use(express.static('dist'));
+app.use(express.static(__dirname + '/dist'));
 
 // send the user to index html page inspite of the url
 app.get('/', (req, res) => {
-  res.sendFile(__dirname+'/dist/index.html');
+  res.sendFile(path.resolve(__dirname + '/dist/index.html'));
 });
 
 app.listen(port, '0.0.0.0',() => console.log(`Serving UI at ${port}!`));
