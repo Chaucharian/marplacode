78734d8fdd4e55a9aa2306812edb569f70b6cae3
Agustin Salinas <chaucharian@gmail.com>
2019-08-01 23:34:20 -0300

    keeping it all simple

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const path = __dirname + '/dist';
-//const path = require('path');
 const port = process.env.PORT || 1234;
 
 router.use(function (req,res,next) {
@@ -12,11 +11,9 @@ router.use(function (req,res,next) {
 
 router.get('/', function(req,res){
   res.sendFile(path + '/index.html');
-//  res.sendFile( path.join(__dirname, '/dist', 'index.html') );
 });
 
 app.use(express.static(path));
-// app.use( express.static( path.join(__dirname, '/dist')) );
 app.use('/', router);
 
 app.listen(port, '0.0.0.0',() => console.log(`Serving UI at ${port}!`));
