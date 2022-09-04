3db1b272aa8887db9e2f983e286b0f0347d40dca
AS <agustin@beemortagageapp.com>
2022-09-04 03:04:28 -0300

    navigation poc 1

--- src/components/dom/BurgerButton.tsx
+++ src/components/dom/BurgerButton.tsx
@@ -1,77 +1,3 @@
-// import React from 'react'
-// import styled from 'styled-components'
-
-// const Container = styled.div`
-//   ${({ width, isOpen }) => `
-
-//   button {
-//     display: block;
-//     background-color: transparent;
-//     outline: none;
-//     width: 50px;
-//     height: 15px;
-//     margin: 0px;
-//     padding: 0px;
-//     position: relative;
-//     transform: rotate(0deg);
-//     transition: 0.5s ease-in-out;
-//     cursor: pointer;
-//     border: none;
-
-//     span {
-//       display: block;
-//       position: absolute;
-//       left: 0px;
-//       height: 2px;
-//       width: 100%;
-//       background: #fff;
-//       border-radius: 9px;
-//       opacity: 1;
-//       transition: 0.25s ease-in-out;
-//       transform-origin: left center;
-
-//       &:hover {
-//         background: grey;
-//       }
-//     }
-
-//     span:nth-child(1) {
-//       top: 0px;
-//     }
-//     span:nth-child(2) {
-//       top: 12px;
-//     }
-//     span:nth-child(3) {
-//       top: 24px;
-//     }
-//       ${
-//         isOpen &&
-//         `
-//     span:nth-child(1) {
-//         transform: rotate(45deg);
-//         top: -3px;
-//     }
-
-//     span:nth-child(2) {
-//         transform: rotate(-45deg);
-//         top: 25px;
-//     }
-//   }`
-//       }
-//   `}
-// `
-
-// const BugerButton = ({ open, show, onClick }: any) => (
-//   <Container isOpen={open} width={show ? '100%' : '0%'} onClick={onClick}>
-//     <button>
-//       <span></span>
-//       <span></span>
-//     </button>
-//   </Container>
-// )
-
-// export default BugerButton
-
 import React, { useState } from 'react'
 import { useSpring, animated } from 'react-spring'
 import styled from 'styled-components'
@@ -89,7 +15,6 @@ const Button = styled.button`
 `
 
 const Svg = styled.svg`
-  mix-blend-mode: difference;
   transition: all ease-in-out 1s;
 `
 
